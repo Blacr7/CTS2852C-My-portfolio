2ac098dab4a2646c58454ec893ae686c0d20cebf
blacr7 <blacr7@mail.broward.edu>
2017-10-01 16:39:55 -0400

    Lab 2 - create a webpage that prompts the user to put in a radius and returns the area of a circle.

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,6 @@
-// Scripting code
-console.log('Starting JavaScript code');
+function circleArea() {
+    var radius = prompt("Please enter the radius of your circle.");
+    var area = Math.PI * radius * radius; 
+
+    document.write(area);
+}
