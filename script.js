f073e4fdf513e38690eff93c055c8a3a4f9c6be5
blacr7 <blacr7@mail.broward.edu>
2017-10-17 18:34:59 -0400

    added input validation, and made the display message cleaner.

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-//list of numbers, and the variable that will hold the math for the program
-var coins = [16, 8, 4, 2, 1];
-var value = 0;
 
-//ask the user for a number
-var number = Number(prompt('please enter a number between 20 and 100.'));
+lab3();
 
-//counting loops and list locations
-var i = 0;
-var count = 0;
-
-//filter out numbers under 20 and over 100
-if (number < 20 || number > 100){
-    alert("Invalid number");
-} 
-
-
-while (value != number ) {
-    //run the loop while a number from the list + the value is less than or equal to the number the user entered 
-    while(coins[i] + value <= number){
-        value += coins[i];
-        console.log(coins[i]);
-
-        //count how many times the loop runs. currently its only doing the first position of the list which seems wrong.
-        if (coins[i] == coins[0]){
-            count++;
-        }
+function lab3() {
+    //list of numbers, and the variable that will hold the math for the program
+    var coins = [16, 8, 4, 2, 1];
+    var value = 0;
+    //ask the user for a number
+    var number = prompt('please enter a number between 20 and 100.');
+    //counting loops and list locations
+    var i = 0;
+    var u = 0;
+    var count = [0, 0, 0, 0, 0];
+    //filter out numbers under 20 and over 100 and non numbers.
+    if (number < 20 || number > 100 || isNaN(number)) {
+        alert("Invalid number");
+        Program.restart();
     }
-    i++;
+    //convert remaining output to numbers
+    number = Number(number);
+    while (value != number) {
+        //run the loop while a number from the list + the value is less than or equal to the number the user entered 
+        while (coins[i] + value <= number) {
+            value += coins[i];
+            count[i]++;
+        }
+        i++;
+    }
+    document.write("You entered the number: " + number);
+    for (u = 0; u < coins.length; u++) {
+        document.write("<br><br> This would require the " + coins[u] + " coin " + count[u] + " time(s)");
+    }
 }
 
-console.log(value);
-console.log(number);
-console.log(i);
-console.log(count);
