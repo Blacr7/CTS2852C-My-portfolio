3fd79f0926ff926ee573d93db379ddae3d21ab32
blacr7 <blacr7@mail.broward.edu>
2017-11-09 12:22:18 -0500

    Coded the first 4 paragraphs to formatt correctly.

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-// Scripting code
-console.log('Starting JavaScript code');
+window.onclick= function load() {
+    document.getElementById("p1").setAttribute("style", "text-align: center");
+    
+    document.getElementById("p2").setAttribute("style", "text-align: right");
+    
+    document.getElementsByClassName("paragraph")[0].setAttribute("style", "color: red; background-color: yellow;");
+
+    document.getElementsByClassName("paragraph")[1].setAttribute("style", "color: cyan; background-color: blue;");
+
+    setInterval(function(){
+        var i = 0
+        var colors = ["red", "white", "blue", "black", "green", "orange"];
+
+        var paragraph5 = document.getElementsByTagName("p")[4].setAttribute("style", "color: red");
+        i++
+    }, 2000);
+}
+    
+
+
+
+    
