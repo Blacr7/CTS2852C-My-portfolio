4ce292bd617312d4f32047f435cd4f0e28269663
blacr7 <blacr7@mail.broward.edu>
2017-11-09 13:26:22 -0500

    Added and coded paragraph 5 working on paragraph 6

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-window.onclick= function load() {
+window.onclick = function load() {
     document.getElementById("p1").setAttribute("style", "text-align: center");
-    
+
     document.getElementById("p2").setAttribute("style", "text-align: right");
-    
+
     document.getElementsByClassName("paragraph")[0].setAttribute("style", "color: red; background-color: yellow;");
 
     document.getElementsByClassName("paragraph")[1].setAttribute("style", "color: cyan; background-color: blue;");
 
-    setInterval(function(){
-        var i = 0
+    var i = 0
+    setInterval(function () {
         var colors = ["red", "white", "blue", "black", "green", "orange"];
 
-        var paragraph5 = document.getElementsByTagName("p")[4].setAttribute("style", "color: red");
+        if (i >= colors.length) {
+            i = 0;
+        }
+
+        var paragraph5 = document.getElementsByTagName("p")[4].style.color = colors[i];
+
         i++
-    }, 2000);
+    }, 3000);
 }
-    
+
+function paragraph6() {
+    //var used = false;
+    //if (used == false) {
+    var p6 = document.createElement("P");
+    document.body.appendChild(p6);
+    //  used = true;
+    //}
+
+}
 
 
 
-    
